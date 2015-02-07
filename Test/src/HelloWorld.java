243fe2d21e93e98b109c65c3a58f24f286504efa
MKaettner <MKaettner@gmx.de>
2015-02-07 02:55:22 +0100

    Initial commit

--- Test/src/HelloWorld.java
+++ Test/src/HelloWorld.java
@@ -1,6 +1,19 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hallo!");
+		
+		int one = 1;
+		int two = 2;
+		
+		int array[] = new int[10];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i+1;
+		}
+		
+		System.out.println(VariousThings.add(one, two));
+		
+		System.out.println(VariousThings.accumulate(array));
+		
+		System.out.println(VariousThings.hashiarrangs(array));
 	}
 }
